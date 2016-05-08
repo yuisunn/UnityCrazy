b61ed17802634860cda04d71044497b52196fb7d
snake <yuisunn@gmail.com>
2016-05-08 18:16:08 +0800

    整理完成

--- UnityFramework/Assets/Action/ActionDefine.cs
+++
@@ -1,3 +0,0 @@
-﻿using UnityEngine;
-using System.Collections; 
-
--- UnityFramework/Assets/Scripts/Unity/Common/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public static class Config{
-
-	//例子
-    public const string MAP_CONFIG = "Config/map.xml";
-}
