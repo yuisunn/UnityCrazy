7539066b7d5d315f034804f16d6dc7f758b0e972
yuisunn <yuisunn@gmail.com>
2016-10-06 16:42:50 +0800

    shader style add cartoon and sketch

---
+++ UnityFramework/Assets/SLCGame/Shader/ScreenEffect/EdgeDetectionByDepth.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SLCGame
+{
+    public class EdgeDetectionByDepth : PostEffectsBase
+    {
+
+        public Shader edgeDetectShader;
+        private Material edgeDetectMaterial = null;
+        public Material material
+        {
+            get {
+                edgeDetectMaterial = CheckShaderAndCreateMaterial(edgeDetectShader, edgeDetectMaterial);
+                return edgeDetectMaterial;
+            }
+        }
+
+        [Range(0.0f, 1.0f)]
+        public float edgesOnly = 0.0f;
+        public Color edgeColor = Color.black;
+        public Color backgroundColor = Color.white;
+        public float sampleDistance = 1.0f;
+        public float sensitivityDepth = 1.0f;
+        public float sensitivityNormals = 1.0f;
+
+
+        void OnEnable()
+        {
+            GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
+        }
+           
+        [ImageEffectOpaque]//只对非透明物体起作用 渲染队列小于 2500的起作用
+        void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            if (material != null)
+            {
+                material.SetFloat("_EdgeOnly", edgesOnly);
+                material.SetColor("_EdgeColor", edgeColor);
+                material.SetColor("_BackgroundColor", backgroundColor);
+                material.SetFloat("_SampleDistance", sampleDistance);
+                material.SetVector("_Sensitivity", new Vector4(sensitivityNormals,sensitivityDepth,0.0f,0.0f)); 
+
+                Graphics.Blit(src, dest, material);
+            }
+            else
+            {
+                Graphics.Blit(src, dest);
+            }
+        }
+    }
+}
---
+++ UnityFramework/Assets/SLCGame/Shader/ScreenEffect/GaussianBlur.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SLCGame
+{
+    public class GaussianBlur : PostEffectsBase
+    { 
+        public Shader gaussianBlurShader;
+        private Material gaussianBlurMaterial = null;
+        public Material material
+        {
+            get
+            {
+                gaussianBlurMaterial = CheckShaderAndCreateMaterial(gaussianBlurShader, gaussianBlurMaterial);
+                return gaussianBlurMaterial;
+            }
+        }
+
+        // Blur iterations - larger number means more blur.
+        [Range(0, 4)]
+        public int iterations = 3;
+
+        // Blur spread for each iteration - larger value means more blur
+        [Range(0.2f, 3.0f)]
+        public float blurSpread = 0.6f;
+
+        [Range(1, 8)]
+        public int downSample = 2;
+
+        void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            if (material != null)
+            {
+                int rtW = src.width / downSample;
+                int rtH = src.height / downSample;
+
+                RenderTexture buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
+                buffer0.filterMode = FilterMode.Bilinear;
+
+                Graphics.Blit(src, buffer0);
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    material.SetFloat("_BlurSize", 1.0f + i * blurSpread);
+
+                    //生成一个指定大小的 图像缓存
+                    RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+                    // Render the vertical pass 放到图像缓存
+                    Graphics.Blit(buffer0, buffer1, material, 0);
+
+                    RenderTexture.ReleaseTemporary(buffer0);
+                    buffer0 = buffer1;
+                    buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+                    // Render the horizontal pass
+                    Graphics.Blit(buffer0, buffer1, material, 1);
+
+                    RenderTexture.ReleaseTemporary(buffer0);
+                    buffer0 = buffer1;
+                }
+
+                Graphics.Blit(buffer0, dest);
+                RenderTexture.ReleaseTemporary(buffer0);
+            }
+            else
+            {
+                Graphics.Blit(src, dest);
+            }
+        }
+    }
+
+}
